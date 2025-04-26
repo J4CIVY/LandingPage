4680ec3251bb9dec6aa594b5a34594cd40fa8086
BSK Motorcycle Team <bskmotorcycleteam@gmail.com>
2025-04-26 08:39:45 -0500

    Version 3.1 - Ver Documentacion de cambios en bskmt.com/changes/docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['Favicon_BSK_Motorcycle_Team_40x40.webp', 'Logo_Letras_Motoclub_BSK_Motorcycle_Team_Blue_192X192.png', 'Logo_Letras_Motoclub_BSK_Motorcycle_Team_White_500X500.png'],
+      manifest: {
+        name: 'BSK Motorcycle Team',
+        short_name: 'BSK MT',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000031',
+        theme_color: '#ffffff',
+        orientation: 'portrait',
+        lang: 'es-CO',
+        icons: [
+          {
+            src: '/Logo_Letras_Motoclub_BSK_Motorcycle_Team_Blue_192X192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/Logo_Letras_Motoclub_BSK_Motorcycle_Team_White_500X500.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
