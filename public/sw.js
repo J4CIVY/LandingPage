bc8c949e59e6163d0f8682436672d892ca8d610f
BSK Motorcycle Team <bskmotorcycleteam@gmail.com>
2025-08-05 23:25:32 -0500

    Delete public directory

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-  // You can add caching strategies here if needed
-});
