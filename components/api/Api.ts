8123bc03c8016cee7e1d948d030973c8afbfd363
BSK Motorcycle Team <bskmotorcycleteam@gmail.com>
2025-08-06 18:05:33 -0500

    Update Api.ts

--- components/api/Api.ts
+++ components/api/Api.ts
@@ -3,10 +3,11 @@ import https from 'https';
 
 // Create an Axios instance with a base URL.
 const api = axios.create({
-  baseURL: 'https://localhost:3001/api/v1',
+  baseURL: 'https://api.bskmt.com/api/v1',
   httpsAgent: new https.Agent({
     rejectUnauthorized: false,
   }),
 });
 
 export default api;
+
